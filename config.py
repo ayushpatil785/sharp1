01149ded8686085197a6c9e5096bed3003bf6ec4
ayushpatil785 <83728431+ayushpatil785@users.noreply.github.com>
2024-11-15 00:59:38 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY SHARP
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7359463565:AAE34EVlmJuxd5-WuKj1z1qrkaUYR0Fv6pI")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("LEGENDMODS")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("6037730087")
+
+
+
+
+
+
+
+  
